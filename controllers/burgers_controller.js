d395caba930495398d224a30d8a45b1d588f42f3
Eric Hamil <erichamil@Erics-MBP.attlocal.net>
2019-11-24 20:47:43 -0500

    created all files and folders

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,65 +1,40 @@
-const express = require("express");
-const router = express.Router(); // Call an Instance of the express.Router(), apply Routes to it, and then Tell the Application to use those Routes
+var express = require("express");
+var router = express.Router();
+var burger = require("../models/burger.js");
 
-// Import the model (burger.js) to use its database functions.
-const burger = require("..");
-
-/////////////////////////////////////////////// /* Get Routes */ //////////////////////////////////////////////////////////
-
-// Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
-    console.log("Route Path Hit");
-    burger.selectAll((data) => {
-      handlebarsObject = {
-        burger: data
-      };
-      console.log("Diplayed Burgers");
-      res.render("index", handlebarsObject);
-    });
-
+// get route -> index
+router.get("/", function (req, res) {
+  res.redirect("/burgers");
 });
 
-/////////////////////////////////////////////// /* Post Routes */ //////////////////////////////////////////////////////////
+router.get("/burgers", function (req, res) {
+  // express callback response by calling burger.selectAllBurger
+  burger.all(function (burgerData) {
+    // wrapper for orm.js that using MySQL query callback will return burger_data, render to index with handlebar
+    res.render("index", { burger_data: burgerData });
+  });
+});
 
-router.post("/api/burger", function(req, res) {
-  console.log("burger Route Hit");
-  burger.insertOne(["burger_name","devoured"], [req.body["burger_name"], req.body.devoured], (result)=>{
-    // Send back the ID of the new quote
+// post route -> back to index
+router.post("/burgers/create", function (req, res) {
+  // takes the request object using it as input for burger.addBurger
+  burger.create(req.body.burger_name, function (result) {
+    // wrapper for orm.js that using MySQL insert callback will return a log to console,
+    // render back to index with handle
     console.log(result);
-    res.json(result);
+    res.redirect("/");
   });
 });
 
-router.put("/api/burger/:id", function(req, res) {
-
-  let burgerID = req.params.id
-  let condition = "id = " + burgerID ;
-
-  console.log("burger Route Hit. ID is "+ burgerID);
-  console.log("Dev is " + req.body.devoured);
-
-  burger.updateOne(["devoured"], [req.body.devoured], condition, (result)=>{
-    // Send back the ID of the new quote
-    console.log("Executing First Declared CallBack");
-    res.json(result);
+// put route -> back to index
+router.put("/burgers/:id", function (req, res) {
+  burger.update(req.params.id, function (result) {
+    // wrapper for orm.js that using MySQL update callback will return a log to console,
+    // render back to index with handle
+    console.log(result);
+    // Send back response and let page reload from .then in Ajax
+    res.sendStatus(200);
   });
 });
 
-
-
-
-
-
-
-
-// Export routes for server.js to use.
 module.exports = router;
-
-
-
-
-
-
-
-
-/////////////////////////////////////////////// /* */ //////////////////////////////////////////////////////////
